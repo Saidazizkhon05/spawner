4dcd74a5d4b8c265b2d3b90bebdc4696e5439bb2
Saidazizkhon05 <smister702@gmail.com>
2026-04-14 13:42:00 +0900

    chore: add system_tray and path_provider dependencies
    - Add system_tray for macOS menu bar integration.
    - Add path_provider for local config storage.
    - Add tray icon asset.
    - Enable UPPER_SNAKE_CASE constants per project conventions.

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,6 +5,10 @@
 import FlutterMacOS
 import Foundation
 
+import path_provider_foundation
+import system_tray
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
+  SystemTrayPlugin.register(with: registry.registrar(forPlugin: "SystemTrayPlugin"))
 }
